54202d607b4a54082ea8145cd8b3e61772d13cd4
Gowtham Prabhakaran <168158679+Gowtham-0511@users.noreply.github.com>
2025-06-20 20:18:33 +0530

    feat: Add ESLint configuration to ignore warnings during builds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // <-- Add this line
+  },
 };
 
 export default nextConfig;
