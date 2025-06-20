cb175814079ef195c5d96916156eeade61a0d7e1
Gowtham Prabhakaran <168158679+Gowtham-0511@users.noreply.github.com>
2025-06-20 20:20:18 +0530

    feat: Update ESLint configuration for improved build process

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true, // <-- Add this line
+    ignoreDuringBuilds: true,
   },
 };
 
